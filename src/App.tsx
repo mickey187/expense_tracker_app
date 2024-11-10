19191123eb1c080e05a7b18ad7df4249d432e470
mickey hailu <mickeyhailu20@gmail.com>
2024-11-10 21:03:19 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import { Button } from "@/components/ui/button"
+import { Sidebar } from './components/ui/sidebar'
+
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <Sidebar/>
+    </div>
+    </>
+  )
+}
+
+export default App
